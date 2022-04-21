c8aa886294be3f386596eedf01c8b5a78b32209b
Caterina Strambio De Castillia <caterina.strambio@umassmed.edu>
2022-04-21 13:35:25 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Microscopy Glossary'
+copyright = '2022, GCaterina Strambio De Castillia'
+author = 'Caterina Strambio De Castillia'
 
 release = '0.1'
 version = '0.1.0'
